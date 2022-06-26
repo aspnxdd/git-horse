5f2ff96f3f60e453dab217777cfc0a9595fb53d3
Arnau Espin <43625217+aspnxdd@users.noreply.github.com>
2022-06-26 22:39:09 +0200

    test2

--- src/components/Layout/FileView.vue
+++ src/components/Layout/FileView.vue
@@ -36,7 +36,6 @@ function getStatusColor(status: GitStatus) {
   if (status == GitStatus.Deleted) return "text-[#bf1b1b]";
   if (status == GitStatus.Unknown) return "text-[#575757]";
   return "text-[#f546fa]";
-  
 }
 
 const colorStatus = getStatusColor(props.status);
